09d05b9d0d61e2b452a58b135004ea01d035886d
Nooowkie <alirahmanzanov2323@gmail.com>
2025-05-25 23:16:21 +0600

    NewHM

---
+++ app.js
@@ -0,0 +1,11 @@
+const color = prompt("Введите цвет светофора (красный, желтый, зеленый)").toLowerCase();
+
+if (color === "красный") {
+    console.log("Остановитесь!");
+} else if (color === "желтый") {
+    console.log("Приготовьтесь!");
+} else if (color === "зеленый") {
+    console.log("Можно ехать!");
+} else {
+    console.log("Ошибка: некорректный цвет светофора.");
+}
